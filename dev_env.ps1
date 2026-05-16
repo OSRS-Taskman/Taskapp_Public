e42e45d603d031863e4e99ae67c55e26c069dcc8
Raphael Mobis Tacla <r.mobis@gmail.com>
2026-05-15 23:07:00 -0300

    add dev env variables script for windows users (we code too)

---
+++ dev_env.ps1
@@ -0,0 +1,5 @@
+$env:FLASK_ENV='development'
+$env:MONGO_URI='mongodb://root:example@localhost:27017/'
+$env:SENDGRID_API_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+$env:SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+$env:SERVER_PORT='8080'
